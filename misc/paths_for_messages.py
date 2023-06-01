68e60c7ed8349e51406bec10402e39c517f66d96
Johnny <132753724+JohnnyQuackson@users.noreply.github.com>
2023-06-01 16:30:30 +0300

    Add files via upload

---
+++ misc/paths_for_messages.py
@@ -0,0 +1,3 @@
+welcome_gif = 'files/wel.webm.mp4'
+opps_gif = "https://64.media.tumblr.com/16ebb145c29d8a86eedeacfc4f6210e4/5f53fc97972e7c5c-3b/s540x810/78543b13e36c76317b40a9cac86c214f5a28b66b.gif"
+oxygen_path = 'files/oxygen.ogg'
